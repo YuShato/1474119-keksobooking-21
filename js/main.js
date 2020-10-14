ce69d27fbfdbfc078010246b90ab486563ca68f9
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2020-10-14 13:04:20 +0300

    Добавляет карточку жилья

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const mapDomRect = map.getBoundingClientRect();
 const ads = [];
 const mapPinsElement = map.querySelector(`.map__pins`);
 const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+const card = document.querySelector(`#card`).content.querySelector(`.map__card`);
 const fragment = document.createDocumentFragment();
 
 const getRandomNumber = function (min, max) {
@@ -40,16 +41,21 @@ const createBooking = function (count) {
       },
 
       offer: {
-        title: `text`,
+        title: `Title text`,
         address: `${addressX}, ${addressY}`,
         price: `${getRandomNumber(100, 1000)}`,
-        type: [`palace`, `flat`, `house`, `bungalo`],
+        type: {
+          palace: `Дворец`,
+          flat: `Квартира`,
+          house: `Дом`,
+          bungalow: `Бунгало`
+        },
         rooms: `${getRandomNumber(1, 10)}`,
         guests: `${getRandomNumber(1, 10)}`,
         checkin: `${getRandomNumber(12, 14)}:00`,
         checkout: `${getRandomNumber(12, 14)}:00`,
-        features: `"wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"`,
-        description: `text`,
+        features: [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`],
+        description: `Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.`,
         photos: `Text Array`
       },
 
@@ -61,6 +67,11 @@ const createBooking = function (count) {
 
     ads[i] = ad;
   }
+};
+
+const randomProperty = function (obj) {
+  let keys = Object.keys(obj);
+  return obj[keys[keys.length * Math.random() << 0]];
 };
 
 const addBookingOnMap = function (array, parentElement) {
@@ -76,3 +87,54 @@ map.classList.remove(`map--faded`);
 addBookingOnMap(ads, fragment);
 
 mapPinsElement.appendChild(fragment);
+
+// task 3.2
+
+const popupTitle = card.querySelector(`.popup__title`);
+const popupTextAdress = card.querySelector(`.popup__text--address`);
+const popupTextPrice = card.querySelector(`.popup__text--price`);
+const popupType = card.querySelector(`.popup__type`);
+const popupCapacity = card.querySelector(`.popup__text--capacity`);
+const popupTime = card.querySelector(`.popup__text--time`);
+const popupDescription = card.querySelector(`.popup__description`);
+const popupPhoto = card.querySelector(`.popup__photo`);
+const popupPhotosContainer = card.querySelector(`.popup__photos`);
+const popupAvatar = card.querySelector(`.popup__avatar`);
+const mapFiltersContainer = map.querySelector(`.map__filters-container`);
+
+mapFiltersContainer.before(card);
+
+const fillPhotoSrc = function () {
+  const photosSrc = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+  setAttributeData(popupPhoto, `src`, photosSrc[0]);
+
+  for (let i = 1; i < photosSrc.length; i++) {
+    const newImgElem = popupPhoto.cloneNode(true);
+    setAttributeData(newImgElem, `src`, photosSrc[i]);
+    popupPhotosContainer.appendChild(newImgElem);
+  }
+};
+
+const randomFeatures = function () {
+  let htmlfeat = document.querySelectorAll(`.popup__feature`);
+
+  for (let i = 0; i < htmlfeat.length; i++) {
+    let itemDeleteIndex = getRandomNumber(0, htmlfeat.length - 1);
+    htmlfeat[itemDeleteIndex].classList.toggle(`hidden`);
+  }
+};
+
+const fillAdCardDescription = function (elem) {
+  popupTitle.textContent = elem.offer.title;
+  popupTextAdress.textContent = elem.offer.adress;
+  popupTextPrice.textContent = `${elem.offer.price}₽/ночь`;
+  popupType.textContent = randomProperty(elem.offer.type);
+  popupCapacity.textContent = `${elem.offer.rooms} комнаты для ${elem.offer.guests} гостей`;
+  popupTime.textContent = `Заезд после ${elem.offer.checkin}, выезд до ${elem.offer.checkout}`;
+  popupDescription.textContent = elem.offer.description;
+  fillPhotoSrc();
+  randomFeatures();
+  setAttributeData(popupAvatar, `src`, elem.author.avatar);
+};
+
+fillAdCardDescription(ads[0]);
