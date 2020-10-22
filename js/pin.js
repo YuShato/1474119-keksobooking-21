020a8047ca7f5beee9dd2e584244092cc9298a7a
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2020-10-22 17:20:59 +0300

    Добавляет таск5-модуль2

--- js/pin.js
+++ js/pin.js
@@ -4,6 +4,7 @@
   const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
   const PIN_HEIGHT = 70;
   const PIN_WIDTH = 25;
+  const mapOverlay = document.querySelector(`.map__overlay`);
 
 
   const renderPin = function (pin) {
@@ -41,10 +42,71 @@
     });
   };
 
+  const moveMainPin = function (pin, input) {
+    pin.addEventListener(`mousedown`, function (evt) {
+      evt.preventDefault();
+
+      let startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      let mapBorderCoords = mapOverlay.getBoundingClientRect();
+      let mapPinMain = document.querySelector(`.map__pin--main`);
+      const findPinPosition = function (pinElem) {
+        return {
+          pinLeftPosition: pinElem.offsetLeft,
+          pinTopPosition: pinElem.offsetTop
+        };
+      };
+
+
+      const onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+
+        let changePinPosition = function (myPin) {
+          let newObj = findPinPosition(myPin);
+          let posLeft = newObj.pinLeftPosition + PIN_WIDTH / 2;
+          let posTop = newObj.pinTopPosition - PIN_HEIGHT;
+          let posRight = newObj.pinLeftPosition + PIN_WIDTH * 6;
+          let shift = {
+            x: startCoords.x - moveEvt.clientX,
+            y: startCoords.y - moveEvt.clientY
+          };
+
+          startCoords = {
+            x: moveEvt.clientX,
+            y: moveEvt.clientY
+          };
+
+          if (posLeft > mapBorderCoords.left && posRight < mapBorderCoords.right && posTop > mapBorderCoords.top && posTop < mapBorderCoords.bottom) {
+            pin.style.top = (pin.offsetTop - shift.y) + `px`;
+            pin.style.left = (pin.offsetLeft - shift.x) + `px`;
+            setPinAdress(pin, input);
+          }
+
+        };
+        changePinPosition(mapPinMain);
+
+      };
+
+      const onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+
+        document.removeEventListener(`mousemove`, onMouseMove);
+        document.removeEventListener(`mouseup`, onMouseUp);
+      };
+
+      document.addEventListener(`mousemove`, onMouseMove);
+      document.addEventListener(`mouseup`, onMouseUp);
+    });
+  };
+
   window.pinModule = {
     renderPin,
     getCoords,
     setPinAdress,
-    activeMapPinMain
+    activeMapPinMain,
+    moveMainPin
   };
 })();
