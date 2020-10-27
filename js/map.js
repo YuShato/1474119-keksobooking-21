b36bb8f40c3bb721d1eb857a6c63d8d89a07be11
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2020-10-27 16:52:47 +0300

    выполняет модуль6-таск1

--- js/map.js
+++ js/map.js
@@ -5,27 +5,51 @@
   const fragment = document.createDocumentFragment();
   const map = document.querySelector(`.map`);
   const adForm = document.querySelector(`.ad-form`);
+  const BOOKING_AMOUNT = 8;
 
-  const addBookingOnMap = function (array, parentElement) {
-    array.forEach(function (item) {
-      parentElement.appendChild(window.pinModule.renderPin(item));
+  const addPinFromData = function (maxCount, array) {
+    for (let i = 0; i < maxCount; i++) {
+      fragment.appendChild(window.pinModule.renderPin(array[i]));
+    }
+  };
+
+  const renderCardFromServerData = function (pinsData) {
+    const card = document.querySelector(`#card`).content.querySelector(`.map__card`);
+    const mapFiltersContainer = document.querySelector(`.map__filters-container`);
+
+    if (pinsData.length > BOOKING_AMOUNT) {
+      addPinFromData(BOOKING_AMOUNT, pinsData);
+    } else {
+      addPinFromData(pinsData.length, pinsData);
+    }
+
+    mapPinsElement.appendChild(fragment);
+    map.classList.remove(`map--faded`);
+    adForm.classList.remove(`ad-form--disabled`);
+    let allPins = document.querySelectorAll(`.map__pin`);
+    mapPinsElement.addEventListener(`click`, function (evt) {
+      for (let i = 0; i < allPins.length; i++) {
+        if (evt.target === allPins[i]) {
+          window.dataModule.fillCardFromServer(pinsData[i - 1]);
+          mapFiltersContainer.before(card);
+        }
+      }
+      window.popupModule.popupClose();
     });
+
   };
 
   const showActivePage = function () {
-    map.classList.remove(`map--faded`);
-    adForm.classList.remove(`ad-form--disabled`);
-    mapPinsElement.appendChild(fragment);
+    window.backend.load(renderCardFromServerData, window.backend.onShowError);
+    window.popupModule.popupClose();
   };
 
   const findButtonSide = function (evt) {
     if (evt.button === 0) {
       showActivePage();
-      window.dataModule.addPinsInfo();
     }
   };
   window.mapModule = {
-    addBookingOnMap,
     showActivePage,
     findButtonSide,
     fragment
--- js/popup.js
+++ js/popup.js
@@ -2,57 +2,19 @@
 
 (function () {
   const card = document.querySelector(`#card`).content.querySelector(`.map__card`);
-  const popupTitle = card.querySelector(`.popup__title`);
-  const popupTextAdress = card.querySelector(`.popup__text--address`);
-  const popupTextPrice = card.querySelector(`.popup__text--price`);
-  const popupType = card.querySelector(`.popup__type`);
-  const popupCapacity = card.querySelector(`.popup__text--capacity`);
-  const popupTime = card.querySelector(`.popup__text--time`);
-  const popupDescription = card.querySelector(`.popup__description`);
-  const popupPhoto = card.querySelector(`.popup__photo`);
-  const popupPhotosContainer = card.querySelector(`.popup__photos`);
-  const popupAvatar = card.querySelector(`.popup__avatar`);
   const mapFiltersContainer = document.querySelector(`.map__filters-container`);
   const mapPins = document.querySelector(`.map__pins`);
-
-
-  const fillPhotoSrc = function () {
-    const photosSrc = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
-    window.util.setAttributeData(popupPhoto, `src`, photosSrc[0]);
-    let findCreatedImg = popupPhotosContainer.querySelectorAll(`.popup__photo`);
-    if (findCreatedImg.length < photosSrc.length) {
-      for (let i = 1; i < photosSrc.length; i++) {
-        const newImgElem = popupPhoto.cloneNode(true);
-        window.util.setAttributeData(newImgElem, `src`, photosSrc[i]);
-        popupPhotosContainer.appendChild(newImgElem);
-      }
-    }
-  };
-
-  const fillAdCardDescription = function (elem) {
-    popupTitle.textContent = elem.offer.title;
-    popupTextAdress.textContent = elem.offer.adress;
-    popupTextPrice.textContent = `${elem.offer.price}₽/ночь`;
-    popupType.textContent = window.util.getRandomProperty(elem.offer.type);
-    popupCapacity.textContent = `${elem.offer.rooms} комнаты для ${elem.offer.guests} гостей`;
-    popupTime.textContent = `Заезд после ${elem.offer.checkin}, выезд до ${elem.offer.checkout}`;
-    popupDescription.textContent = elem.offer.description;
-    fillPhotoSrc();
-    window.dataModule.randomFeatures();
-    window.util.setAttributeData(popupAvatar, `src`, elem.author.avatar);
-  };
 
   const popupClose = function () {
     let currentCard = document.querySelector(`.map__card`);
     currentCard.classList.remove(`visually-hidden`);
-    let buttonClose = document.querySelector(`.popup__close`);
+    let buttonClose = currentCard.querySelector(`.popup__close`);
     buttonClose.addEventListener(`click`, function () {
       currentCard.classList.add(`visually-hidden`);
     });
   };
 
-  const openCardInfo = function (currentIndex) {
-    fillAdCardDescription(window.dataModule.ads[currentIndex - 1]);
+  const openCardInfo = function () {
     mapFiltersContainer.before(card);
     popupClose();
   };
@@ -69,7 +31,8 @@
 
   window.popupModule = {
     openCardInfo,
-    closeCardEscButton
+    closeCardEscButton,
+    popupClose
   };
 })();
 
